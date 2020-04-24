1763b37728a610955f8feb744be2bf6ea67bb691
HardDie <oleg1995sysoev@yandex.ru>
2020-04-25 00:38:10 +0300

    Add flags, refactoring

--- main.go
+++ main.go
@@ -13,21 +13,38 @@ import (
 	"time"
 )
 
-var logEnabled bool = false
-
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func connectToFtp(server, user, pass string) (*ftp.ServerConn, error) {
-	ftpClient, err := ftp.Dial(server, ftp.DialWithTimeout(5*time.Second))
+func connectToFtp() (*ftp.ServerConn, error) {
+	homeDir := os.Getenv("HOME")
+	if len(homeDir) == 0 {
+		return nil, errors.New("$HOME variable is empty")
+	}
+
+	server, err := readDataFromFile(homeDir + "/.ftp/server")
+	if err != nil {
+		return nil, errors.New("can't read server")
+	}
+	login, err := readDataFromFile(homeDir + "/.ftp/login")
+	if err != nil {
+		return nil, errors.New("can't read login")
+	}
+
+	pass, err := readDataFromFile(homeDir + "/.ftp/pass")
+	if err != nil {
+		return nil, errors.New("can't read pass")
+	}
+
+	ftpClient, err := ftp.Dial(server+":21", ftp.DialWithTimeout(5*time.Second))
 	if err != nil {
 		return nil, errors.New("can't connect to server")
 	}
 
-	if err := ftpClient.Login(user, pass); err != nil {
+	if err := ftpClient.Login(login, pass); err != nil {
 		return nil, errors.New("can't auth on server")
 	}
 
@@ -50,7 +67,7 @@ func downloadFile(ftpClient *ftp.ServerConn, filename string, size uint64) error
 		bar_len := int(size.Col) - len(filename) - len(progress) - 3
 
 		bar := ioprogress.DrawTextFormatBar(int64(bar_len))
-		return fmt.Sprintf("%s: %s %s\n", filename, bar(a, b), progress)
+		return fmt.Sprintf("%s: %s %s", filename, bar(a, b), progress)
 	}
 	progressR := &ioprogress.Reader{
 		Reader:   data,
@@ -118,62 +135,85 @@ func calcMD5(filename string) string {
 	return fmt.Sprintf("%x", md5.Sum(dat))
 }
 
+func byteUnitStr(n uint64) string {
+	var byteUnits = []string{"B", "KB", "MB", "GB", "TB", "PB"}
+
+	var unit string
+	size := float64(n)
+	for i := 1; i < len(byteUnits); i++ {
+		if size < 1000 {
+			unit = byteUnits[i-1]
+			break
+		}
+
+		size = size / 1000
+	}
+
+	return fmt.Sprintf("%.3g %s", size, unit)
+}
+
 func main() {
+	var fileslist []string
+	f_removeOnly := false
+	f_list := false
+
 	/**
-	 * Read variables
+	 * Connect to server
 	 */
-	homeDir := os.Getenv("HOME")
-	if len(homeDir) == 0 {
-		log.Fatal("$HOME variable is empty")
-	}
-
-	server, err := readDataFromFile(homeDir + "/.ftp/server")
-	check(err)
-	login, err := readDataFromFile(homeDir + "/.ftp/login")
-	check(err)
-	pass, err := readDataFromFile(homeDir + "/.ftp/pass")
+	client, err := connectToFtp()
 	check(err)
 
-	var channels []chan bool
-
-//	if len(os.Args) == 2 {
-		logEnabled = true
-//	}
-
-	for i := 1; i < len(os.Args); i++ {
-		loc_chan := make(chan bool)
-		channels = append(channels, loc_chan)
-		filename := os.Args[i]
-		go func() {
-			client, err := connectToFtp(server+":21", login, pass)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if logEnabled {
-				fmt.Printf("%v: Wait...\r", filename)
-			}
-			size, err := waitForFile(client, filename)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if err := downloadFile(client, filename, size); err != nil {
-				log.Fatal(err)
-			}
-			if err := deleteFile(client, filename); err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("%s: Done! md5sum = %s\n", filename, calcMD5(filename))
-
-			if err := client.Quit(); err != nil {
-				log.Fatal(err)
-			}
-
-			loc_chan <- true
-		}()
+	/**
+	 * Parse arguments
+	 */
+	for _, token := range os.Args[1:] {
+		if token == "-d" {
+			f_removeOnly = true
+			continue
+		} else if token == "-l" {
+			f_list = true
+			continue
+		}
+		fileslist = append(fileslist, token)
 	}
 
-	for _, channel := range channels {
-		<-channel
+	/**
+	 * Show list files
+	 */
+	if f_list {
+		entries, err := client.List("/")
+		if err != nil {
+			log.Fatalf("Can't get list of files")
+		}
+		for _, entry := range entries {
+			fmt.Printf("%10s - %s\n", byteUnitStr(entry.Size), entry.Name)
+		}
 	}
+
+	/**
+	 * Download all files one by one
+	 */
+	for _, file := range fileslist {
+		fmt.Printf("%v: Wait...\r", file)
+		size, err := waitForFile(client, file)
+		check(err)
+
+		if !f_removeOnly {
+			err = downloadFile(client, file, size)
+			check(err)
+		}
+
+		err = deleteFile(client, file)
+		check(err)
+
+		if !f_removeOnly {
+			fmt.Printf("%s: Done! md5sum = %s\n", file, calcMD5(file))
+		}
+	}
+
+	/**
+	 * Quit
+	 */
+	err = client.Quit()
+	check(err)
 }
